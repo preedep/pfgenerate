bc5966d63d964427505679902ce8f5bbb7b40b16
Preedee Ponchevin <nickmsft@mac.com>
2023-11-22 20:23:14 +0700

    Add   - Add page   - Add static web

--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,5 +1,6 @@
 use actix_session::Session;
 use actix_web::{HttpResponse, Responder, web};
+use handlebars::Handlebars;
 use log::debug;
 use tracing_attributes::instrument;
 
@@ -11,6 +12,7 @@ use crate::utils::utils::KEY_ID_TOKEN;
 pub async fn page_index(
     session: Session,
     data: web::Data<Config>,
+    hb: web::Data<Handlebars<'_>>,
 ) -> impl Responder {
     match session.get::<IDToken>(KEY_ID_TOKEN).unwrap() {
         None => {}
@@ -18,5 +20,44 @@ pub async fn page_index(
             debug!("Page Index with ID token {:#?}", id_token);
         }
     }
-    HttpResponse::Ok().finish()
+    let x = IDToken{
+        aud: None,
+        iss: None,
+        iat: None,
+        nbf: None,
+        exp: None,
+        acct: None,
+        acrs: None,
+        aio: None,
+        auth_time: None,
+        ctry: None,
+        email: None,
+        family_name: None,
+        given_name: None,
+        groups: None,
+        idp: None,
+        ipaddr: None,
+        login_hint: None,
+        name: None,
+        nonce: None,
+        oid: None,
+        preferred_username: None,
+        rh: None,
+        sid: None,
+        sub: None,
+        tenant_ctry: None,
+        tenant_region_scope: None,
+        tid: None,
+        uti: None,
+        ver: None,
+        wids: None,
+        xms_pl: None,
+        xms_tpl: None,
+        employee_id: None,
+        department: None,
+        companyname: None,
+        officelocation: None,
+    };
+    let body = hb.render("index", &x).unwrap();
+    HttpResponse::Ok().body(body)
 }
